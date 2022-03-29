81eac8306e97b2a4b04aa563d00f096cb5f868b9
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2022-03-29 17:23:04 +0700

    add get all tasks api

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = []; //if dont make it private, any component from outside can change this array
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
